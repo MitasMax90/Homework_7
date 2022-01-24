2475fe89962c9a80d2e0213093180c0605bc5770
Администратор <rostishka90@mail.ru>
2022-01-25 00:09:54 +0300

    Third commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         Commander commander = new Commander();
-        MySoldier mySoldier = new MySoldier();
-        commander.force(mySoldier, 14);
+        /* Реализуем работу через анонимный класс,
+         для этого не нужно будет создавать класс MySoldier и объект этого класса */
+        commander.force(new Soldier() {
+            @Override
+            public String workingDays(int countOfDays) {
+                return "Я кошу траву " + countOfDays + " дней";
+            }
+        }, 17);
     }
 }
